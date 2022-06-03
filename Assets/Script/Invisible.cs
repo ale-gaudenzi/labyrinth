3b33a5055bee0ecb1e6dedd3f9097ea0a3c00968
ale-gaudenzi <alegau97@gmail.com>
2022-06-03 11:44:15 +0200

    invisibles

---
+++ Assets/Script/Invisible.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Invisible : MonoBehaviour
+{
+    public Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+
+    // Toggle the Object's visibility each second.
+    void Update()
+    {
+        // Find out whether current second is odd or even
+        ///bool oddeven = Mathf.FloorToInt(Time.time) % 2 == 0;
+
+        // Enable renderer accordingly
+       // rend.enabled = oddeven;
+    }
+}
